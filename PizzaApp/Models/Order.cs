e41b2dfdeb50300a45b903fba730f1bf8afe2048
Leon Moore <lmoore0621@gmail.com>
2023-02-04 16:47:45 -0600

    Add project files.

---
+++ PizzaApp/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public DateTime? OrderFulfilled { get; set; }
+        public Customer Customer { get; set; } = null;
+
+        public ICollection<OrderDetail> OrderDetails { get; set; } = null;
+    }
+}
---
+++ PizzaApp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null;
+
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+    }
+}
