2bea6a8a7ae4dfef98f20d33f72bd15c80cf9b4a
Leon Moore <lmoore0621@gmail.com>
2023-02-04 19:19:51 -0600

    add data to newly created tables

---
+++ PizzaApp/Data/PizzaAppContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaApp.Data
+{
+    public class PizzaAppContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Data Source=WIN-A9PGGCOIFI4;Initial Catalog=PizzaApp;Integrated Security=True;TrustServerCertificate=true");
+        }
+    }
+}
